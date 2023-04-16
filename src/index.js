3a4c18d9755c23964dc16511c7e7a69dcc01270c
Ebla Ekerdiker <eblaekerdiker@gmail.com>
2023-04-16 15:24:10 +0300

    Completed assignment with one problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("form");
+  const submit = document.querySelector("#submit");
+  submit.addEventListener('click', submitForm)
+  function submitForm(e){
+    e.preventDefault();
+    const newTask = document.getElementById("new-task-description").value;
+    const list = document.getElementById("list-item");
+    list.textContent += newTask;
+  }   
+
 });
